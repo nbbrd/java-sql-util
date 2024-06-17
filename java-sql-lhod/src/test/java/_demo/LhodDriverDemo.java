ea558d0b8109106e210acfcb48bfbd8e262afde4
Philippe Charles <philippe.charles@nbb.be>
2024-06-17 09:35:29 +0200

    Simplify demo

--- java-sql-lhod/src/test/java/_demo/LhodDriverDemo.java
+++ java-sql-lhod/src/test/java/_demo/LhodDriverDemo.java
@@ -1,37 +1,35 @@
 /*
  * Copyright 2018 National Bank of Belgium
- * 
- * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
  * by the European Commission - subsequent versions of the EUPL (the "Licence");
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl
- * 
- * Unless required by applicable law or agreed to in writing, software 
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and 
+ * See the Licence for the specific language governing permissions and
  * limitations under the Licence.
  */
 package _demo;
 
 import internal.sql.lhod.LhodDriver;
-import nbbrd.sql.odbc.OdbcDataSource;
-import nbbrd.sql.odbc.OdbcRegistry;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import nbbrd.sql.jdbc.SqlColumn;
 import nbbrd.sql.jdbc.SqlTable;
+import nbbrd.sql.odbc.OdbcDataSource;
+import nbbrd.sql.odbc.OdbcRegistry;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import static java.util.Comparator.comparing;
 
 /**
- *
  * @author Philippe Charles
  */
 public class LhodDriverDemo {
@@ -39,19 +37,19 @@ public class LhodDriverDemo {
     public static void main(String[] args) throws IOException, SQLException {
         OdbcRegistry registry = OdbcRegistry.ofServiceLoader()
                 .orElseThrow(UnsupportedOperationException::new);
-        System.out.println("registry '" + registry.getName() + "'");
+        System.out.println("   Registry: '" + registry.getName() + "'");
 
         OdbcDataSource source = registry.getDataSources(OdbcDataSource.Type.USER)
                 .stream()
-                .findFirst()
+                .min(comparing(OdbcDataSource::getName))
                 .orElseThrow(NoSuchElementException::new);
-        System.out.println("source '" + source.getName() + "'");
+        System.out.println("     Source: '" + source.getName() + "'");
 
         SqlTable table = getFirstTable(source).orElseThrow(NoSuchElementException::new);
-        System.out.println(table);
+        System.out.println(" FirstTable: " + table);
 
         SqlColumn column = getFirstColumn(source, table).orElseThrow(NoSuchElementException::new);
-        System.out.println(column);
+        System.out.println("FirstColumn: " + column);
     }
 
     private static Optional<SqlTable> getFirstTable(OdbcDataSource source) throws SQLException {
